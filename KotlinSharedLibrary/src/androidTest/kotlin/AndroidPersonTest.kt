88f818be8926cc664415212c4b533e88667e1993
Alexandre Bonfitto <alexandre.bonfitto@dextra-sw.com>
2019-06-25 16:23:48 -0300

    Configured JaCoCo test coverage reports for KotlinSharedLibrary.

--- KotlinSharedLibrary/src/androidTest/kotlin/AndroidPersonTest.kt
+++ KotlinSharedLibrary/src/androidTest/kotlin/AndroidPersonTest.kt
@@ -1,22 +1,16 @@
-import br.alexandrehtrb.kotlinsharedlibrary.Parcelable
 import br.alexandrehtrb.kotlinsharedlibrary.Person
-
-import java.io.Serializable
-
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class AndroidPersonTest {
 
     @Test
-    fun testAndroidPerson() {
+    fun testPerson_createApplicationScreenMessage() {
         val person = Person("John", 20)
-        Assert.assertEquals(3, 1 + 2)
-        Assert.assertEquals("John", person.name)
-        Assert.assertEquals(20, person.age)
-        Assert.assertTrue(person.platformName.contains("Android", ignoreCase = false))
-        Assert.assertTrue(person is Serializable)
-        Assert.assertTrue(person is Parcelable)
+        assertEquals("John", person.name)
+        assertEquals(20, person.age)
+        val message = "John, 20 years old, is using Kotlin Multiplatform in Android"
+        assertTrue(person.createApplicationScreenMessage().contains(message))
     }
-
 }
