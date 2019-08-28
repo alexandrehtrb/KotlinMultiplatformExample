3260bafd3b6dcfdfec9428ca000146b7f368472c
Alexandre Bonfitto <alexandre.bonfitto@dextra-sw.com>
2019-08-28 17:43:10 -0300

    Updated Kotlin version and removed Kotlin Native Gradle Plugin

--- KotlinSharedAndroid/build.gradle.kts
+++ KotlinSharedAndroid/build.gradle.kts
@@ -11,7 +11,6 @@ buildscript {
         classpath(Plugins.androidGradlePlugin)
         classpath(Plugins.androidJacocoPlugin)
         classpath(Plugins.kotlinGradlePlugin)
-        classpath(Plugins.kotlinNativeGradlePlugin)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
--- KotlinSharedAndroid/buildSrc/src/main/kotlin/Plugins.kt
+++ KotlinSharedAndroid/buildSrc/src/main/kotlin/Plugins.kt
@@ -1,9 +1,8 @@
 object Plugins {
-    const val kotlinVersion = "1.3.41"
+    const val kotlinVersion = "1.3.50"
     const val jacocoVersion = "0.8.3"
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:3.6.0-alpha06"
     const val androidJacocoPlugin = "com.vanniktech:gradle-android-junit-jacoco-plugin:0.15.0"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
-    const val kotlinNativeGradlePlugin = "org.jetbrains.kotlin:kotlin-native-gradle-plugin:$kotlinVersion"
 }
